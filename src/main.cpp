03d20763cbbb72ab7c1db4d6ce43a68af84e3019
Lucas Pereira <lucaspereira0612@gmail.com>
2017-08-18 04:17:07 -0300

    somewhat working code - double check FSM before submission

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -174,6 +175,10 @@ int main() {
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
 
+  //bool keep_lane = true;
+  //bool change_lane_left = false;
+  //bool change_lane_right = false;
+
   ifstream in_map_(map_file_.c_str(), ifstream::in);
 
   string line;
@@ -232,13 +237,195 @@ int main() {
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
-
-          	json msgJson;
+			
+			auto current_lane = floor(car_d/4.0);
+			auto current_speed = car_speed;
+			
+			bool traffic_ahead = false;
+			bool traffic_left_frt = false;
+			bool traffic_left_bck = false;
+			bool traffic_right_frt = false;
+			bool traffic_right_bck = false;
+			
+			auto prev_path_size = previous_path_x.size();
+			auto ref_y = car_y;
+			auto ref_x = car_x;
+			auto ref_yaw = car_yaw;
+			auto ref_s = car_s;
+			auto prev_ref_x = car_x - cos(car_yaw);
+			auto prev_ref_y = car_y - sin(car_yaw);
+			if(prev_path_size >= 2){
+				prev_ref_x = previous_path_x[prev_path_size - 2];
+				prev_ref_y = previous_path_y[prev_path_size - 2];
+				ref_x = previous_path_x[prev_path_size - 1];
+				ref_y = previous_path_y[prev_path_size - 1];
+				ref_yaw = atan2(ref_y - prev_ref_y, ref_x - prev_ref_x);
+				ref_s = end_path_s;
+				current_lane = floor(end_path_d/4.0);
+				current_speed = distance((ref_x - prev_ref_x), (ref_y - prev_ref_y), 0, 0);
+				current_speed = current_speed/0.02;
+			}
+			
+			double min_diff_ahead = 100000;
+			double min_diff_left_frt = 100000;
+			double min_diff_left_bck = 100000;
+			double min_diff_right_frt = 100000;
+			double min_diff_right_bck = 100000;
+			
+			double speed_ahead = 22;
+			double speed_left_frt = 22;
+			double speed_left_bck = 22;
+			double speed_right_frt = 22;
+			double speed_right_bck = 22;
+			
+			double target_lane = current_lane;
+			double target_speed = current_speed;
+			
+			for(auto i = 0; i < sensor_fusion.size(); i++){
+				vector<double> vehicle = sensor_fusion[i];
+				auto vehicle_speed = distance(vehicle[3], vehicle[4], 0, 0);
+				auto vehicle_lane = floor(vehicle[6]/4.0);
+				auto vehicle_s = vehicle[5];
+				
+				auto vehicle_s_fwd = vehicle_s + prev_path_size*0.02*vehicle_speed;
+				
+				auto s_diff = vehicle_s_fwd - ref_s;
+				
+				if(vehicle_lane < current_lane){
+					if(s_diff > -5 && s_diff < 40){
+						if(s_diff < 25){
+							if(s_diff > 0){
+								if(s_diff < min_diff_left_frt){
+									min_diff_left_frt = s_diff;
+									speed_left_frt = vehicle_speed;
+									traffic_left_frt = true;
+								}
+							} else {
+								if(-s_diff < min_diff_left_bck){
+									min_diff_left_bck = -s_diff;
+									speed_left_bck = vehicle_speed;
+									traffic_left_bck = true;
+								}
+							}
+						} else {
+							//if(s_diff < min_diff_left_frt){
+							//	min_diff_left_frt = s_diff;
+							//	speed_left_frt = vehicle_speed;
+							//	traffic_left_frt = true;
+							//}
+						}
+					}
+				} else {
+					if(vehicle_lane > current_lane){
+						if(s_diff > -5 && s_diff < 40){
+							if(s_diff < 25){
+								if(s_diff > 0){
+									if(s_diff < min_diff_right_frt){
+										min_diff_right_frt = s_diff;
+										speed_right_frt = vehicle_speed;
+										traffic_right_frt = true;
+									}
+								} else {
+									if(-s_diff < min_diff_right_bck){
+										min_diff_right_bck = -s_diff;
+										speed_right_bck = vehicle_speed;
+										traffic_right_bck = true;
+									}
+								}
+							} else {
+								//if(s_diff < min_diff_right_frt){
+								//	min_diff_right_frt = s_diff;
+								//	speed_right_frt = vehicle_speed;
+								//	traffic_right_frt = true;
+								//}
+							}
+						}
+					} else {
+						if(s_diff > 0 && s_diff < 25){
+							if(s_diff < min_diff_ahead){
+								min_diff_ahead = s_diff;
+								speed_ahead = vehicle_speed;
+								traffic_ahead = true;
+							}
+						}
+					}
+				}
+			}
+			
+			target_speed = speed_ahead;
+			
+			if(traffic_ahead){
+				if(current_lane > 0 && !traffic_left_frt && !traffic_left_bck){
+					target_lane = current_lane - 1;
+					target_speed = speed_left_frt;
+				}
+				if(current_lane < 2 && !traffic_right_frt && !traffic_right_bck){
+					target_lane = current_lane + 1;
+					target_speed = speed_right_frt;
+				}
+			}
+			
+			vector<double> spline_ptsx;
+			vector<double> spline_ptsy;
+			
+			spline_ptsx.push_back(prev_ref_x);
+			spline_ptsx.push_back(ref_x);
+			spline_ptsy.push_back(prev_ref_y);
+			spline_ptsy.push_back(ref_y);
+			
+			vector<double> next_spline_pt0 = getXY(ref_s + 30, (2 + 4*target_lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_spline_pt1 = getXY(ref_s + 60, (2 + 4*target_lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			
+			spline_ptsx.push_back(next_spline_pt0[0]);
+			spline_ptsx.push_back(next_spline_pt1[0]);
+			
+			spline_ptsy.push_back(next_spline_pt0[1]);
+			spline_ptsy.push_back(next_spline_pt1[1]);
+			
+			for(auto i = 0; i < spline_ptsx.size(); i++){
+				double shift_x = spline_ptsx[i] - ref_x;
+				double shift_y = spline_ptsy[i] - ref_y;
+				
+				spline_ptsx[i] = (shift_x*cos(-ref_yaw) - shift_y*sin(-ref_yaw));
+				spline_ptsy[i] = (shift_x*sin(-ref_yaw) + shift_y*cos(-ref_yaw));
+			}
+			
+			tk::spline s;
+			
+			s.set_points(spline_ptsx, spline_ptsy);
 
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
+			
+			for(auto i = 0; i < prev_path_size; i++){
+				next_x_vals.push_back(previous_path_x[i]);
+				next_y_vals.push_back(previous_path_y[i]);
+			}
+			
+			double current_x = 0;
+			
+			for(auto i = 0; i < 50 - prev_path_size; i++){
+				if(current_speed < target_speed){
+					current_speed = min(target_speed, current_speed + 0.1);
+				} else {
+					current_speed = max(target_speed, current_speed - 0.1);
+				}
+				double target_x = current_x + current_speed*0.02;
+				double target_y = s(target_x);
+				
+				current_x = target_x;
+				
+				double x_val = (target_x*cos(ref_yaw) - target_y*sin(ref_yaw));
+				double y_val = (target_x*sin(ref_yaw) + target_y*cos(ref_yaw));
+				
+				x_val += ref_x;
+				y_val += ref_y;
+				
+				next_x_vals.push_back(x_val);
+				next_y_vals.push_back(y_val);
+			}
 
-
+			json msgJson;
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
